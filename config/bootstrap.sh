11bb9936b2da836e05c67e2a904ee50d2aa7323a
Larry Smith Jr <mrlesmithjr@gmail.com>
2021-11-24 20:37:55 +0000

    Moved script to clean up root

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -4,8 +4,8 @@
 if [ -f /etc/debian_version ]; then
     apt-get update -y &&
         apt-get install -y --no-install-recommends curl fontconfig fonts-font-awesome \
-        git libffi-dev openssh-client python3-pip python3-setuptools ssh sudo systemd \
-        vim wget zsh &&
+            git libffi-dev openssh-client python3-pip python3-setuptools ssh sudo systemd \
+            vim wget zsh &&
         apt-get clean &&
         rm -rf /var/lib/apt/lists/*
 fi
